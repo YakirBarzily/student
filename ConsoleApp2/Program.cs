18755b123c80e38ee3cab2602628a8b57e1078e3
User <User@COMP-6>
2022-02-10 16:25:36 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int q;
+            Console.WriteLine("please enter a number of student");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter a number of chairs");
+            y = int.Parse(Console.ReadLine());
+            q = (x - y)
+        }
+    }
+}
